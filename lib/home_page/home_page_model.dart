367eb99bc3ac9dc4cabc24c278204a3bf6ff69c6
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 18:45:31 -0500

    Update home_page_model.dart

--- lib/home_page/home_page_model.dart
+++ lib/home_page/home_page_model.dart
@@ -1,43 +1,44 @@
+import '/auth/supabase_auth/auth_util.dart';
 import '/backend/supabase/supabase.dart';
-import '/components/side_bar_nav_widget.dart';
+import '/components/app_title/app_title_widget.dart';
+import '/components/create_exercise/create_exercise_widget.dart';
+import '/components/exercise_on_tap/exercise_on_tap_widget.dart';
+import '/components/paid_plan_prompt/paid_plan_prompt_widget.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:math';
 import 'dart:async';
 import 'home_page_widget.dart' show HomePageWidget;
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // Model for sideBarNav component.
-  late SideBarNavModel sideBarNavModel;
-  Completer<List<CustomersRow>>? requestCompleter;
+  // Model for AppTitle component.
+  late AppTitleModel appTitleModel;
+  Completer<List<ExercisesRow>>? requestCompleter;
 
-  /// Initialization and disposal methods.
-
+  @override
   void initState(BuildContext context) {
-    sideBarNavModel = createModel(context, () => SideBarNavModel());
+    appTitleModel = createModel(context, () => AppTitleModel());
   }
 
+  @override
   void dispose() {
     unfocusNode.dispose();
-    sideBarNavModel.dispose();
+    appTitleModel.dispose();
   }
 
-  /// Action blocks are added here.
-
-  /// Additional helper methods are added here.
-
+  /// Additional helper methods.
   Future waitForRequestCompleted({
     double minWait = 0,
     double maxWait = double.infinity,
